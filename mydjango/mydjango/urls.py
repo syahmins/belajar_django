0dad8ad797efe94fdb4077f45badcd9a25b910b4
syahmins <syahmin@gmail.com>
2020-05-23 15:20:55 +0700

    Initial django

--- mydjango/mydjango/urls.py
+++ mydjango/mydjango/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from myapp import views
+
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
+
 ]
